d3ad5e5388247a7c9645f9cce653b6e7ddfe414b
manxcatrumpy <manxcatrumpy@gmail.com>
2026-09-10 22:51:08 +0800

    chore: bump 至 1.4.1 (build 58)
    畫面版號與 SW cache 跟著 version.js。

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // 出貨只改 APP_SEMVER 與 APP_BUILD。畫面版號與 SW cache 都從這裡來。
 // package.json 的 "version" 請跟 APP_SEMVER 一致。
 var APP_SEMVER = '1.4.1';
-var APP_BUILD = 57;
+var APP_BUILD = 58;
 var APP_CACHE_SUFFIX = '';
